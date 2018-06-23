77fdcac3d0c55252929b4f2a80dec2f6fe3b51f7
Etai Plushnick <etplush@gmail.com>
2018-06-22 22:57:23 -0400

    Add py3 and ios directories

---
+++ ios/grade_finder/grade_finderTests/grade_finderTests.swift
@@ -0,0 +1,36 @@
+//
+//  grade_finderTests.swift
+//  grade_finderTests
+//
+//  Created by Etai Plushnick on 6/22/18.
+//  Copyright © 2018 Etai Plushnick. All rights reserved.
+//
+
+import XCTest
+@testable import grade_finder
+
+class grade_finderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
